8275643ba36eb4b8c2f7c297e305019dbfc037d7
hammadmanjotha <54828352+hammadmanjotha@users.noreply.github.com>
2021-03-08 20:44:52 +0500

    New Update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 function ageInDays() {
     let birthYear = prompt("What Year You Born. My Friend!!");
     let answer= (2021 - birthYear )*365;
-    let hours= answer * 24;
     let h1= document.createElement('h1');
     let finalAnswer= document.createTextNode("You Are " + answer +" Days Old");
     h1.setAttribute('id','ageInFinal');
